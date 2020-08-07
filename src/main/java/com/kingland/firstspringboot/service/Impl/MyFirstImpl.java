a172f477ff2495d60273e56728bd9131cef328cd
Steven-Chuang-Yuan <Steven.Chuang.Yuan@Kinfland.com>
2020-08-07 11:10:03 +0000

    mybatis

---
+++ src/main/java/com/kingland/firstspringboot/service/Impl/MyFirstImpl.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright 2020 Kingland Systems Corporation.All Right Reserved
+ */
+package com.kingland.firstspringboot.service.Impl;
+
+import com.kingland.firstspringboot.dao.MyFirstDao;
+import com.kingland.firstspringboot.domain.User;
+import com.kingland.firstspringboot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+/**
+ * @author KSC
+ *
+ */
+@Service
+public class MyFirstImpl implements UserService {
+    @Autowired
+    private MyFirstDao myFirstDao;
+
+    public User findUserByName(String cityName) {
+        return myFirstDao.findByName(cityName);
+    }
+}
---
+++ src/main/resources/db/migration/V2__database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE public.user2
+(
+    name character(10),
+    score character(10),
+    age integer
+);
+
+ALTER TABLE public.user2
+    OWNER to postgres;
